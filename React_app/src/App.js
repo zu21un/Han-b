cffd22d415df79f57ff135bc27bb9926d3c4b1ad
zu21un <jwjoo03@gmail.com>
2021-11-17 11:09:03 +0900

    Feat: 타이틀, 검색창, 키워드 추천 UI 추가

---
+++ React_app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import KeywordMain from './KeywordMain';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <KeywordMain />
+      </header>
+    </div>
+  );
+}
+
+export default App;
