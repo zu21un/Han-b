4fe4b22544af2d3572a4ce33ce220a4f4299c0fe
zu21un <jwjoo03@gmail.com>
2021-12-02 18:46:16 +0900

    Feat: Searchbar에 입력한 내용을 keyword 변수로 관리

--- React_app/src/page/Main/Search.js
+++ React_app/src/page/Main/Search.js
@@ -14,7 +14,12 @@ import SearchIcon from '@mui/icons-material/Search';
 export default function Search() {
   const [keyword, setKeyword] = useState('');
 
-  
+  const handleChange = ({ target: { value } }) => setKeyword(value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(keyword)
+  }
 
   return (
     <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
@@ -25,27 +30,37 @@ export default function Search() {
         sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
       >
         <Toolbar>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item>
-              <SearchIcon color="inherit" sx={{ display: 'block' }} />
+            <Grid 
+            component="form" 
+            onSubmit={handleSubmit} 
+            container 
+            spacing={2} 
+            alignItems="center"
+            > 
+              <Grid item>
+                <SearchIcon color="inherit" sx={{ display: 'block' }} />
+              </Grid>
+              <Grid item xs>
+                <TextField
+                  fullWidth
+                  placeholder="키워드를 검색하세요"
+                  InputProps={{
+                    disableUnderline: true,
+                    sx: { fontSize: 'default', mt: 0.5 },
+                  }}
+                  variant="standard"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item>
+                <Button 
+                  type="submit"
+                  variant="contained" sx={{ mr: 1 }}
+                  onSubmit={handleSubmit}>
+                  검 색
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs>
-              <TextField
-                fullWidth
-                placeholder="키워드를 검색하세요"
-                InputProps={{
-                  disableUnderline: true,
-                  sx: { fontSize: 'default', mt: 1 },
-                }}
-                variant="standard"
-              />
-            </Grid>
-            <Grid item>
-              <Button variant="contained" sx={{ mr: 1 }}>
-                검 색
-              </Button>
-            </Grid>
-          </Grid>
         </Toolbar>
       </AppBar>
       <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
