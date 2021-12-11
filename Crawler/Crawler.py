d2160c674fd1145dd6b75565d549227c3cf2898b
Kim Yong Seong <yongseong97@naver.com>
2021-12-11 15:47:19 +0900

    Feat: db에 item 넣기추가

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -10,9 +10,19 @@ import boto3
 import traceback
 
 class Information:
-    def __init__(self, hypertitle, title):
+
+#이렇게 만들면 터질거같음. 
+    total_hypertitle = []
+    total_title = []
+
+    def __init__(self, hypertitle, title, organization):
         self.hypertitle = hypertitle
         self.title = title
+        self.organization = organization
+    
+    # def load(self, hypertitle, title):
+
+
 
 
 class Crawler:
@@ -26,7 +36,7 @@ class Crawler:
         req = Request("http://cs.hanyang.ac.kr/board/info_board.php")# urllib.request 데이터를 보낼 때 인코딩하여 바이너리 형태로 보낸다 없는 페이지를 요청해도 에러를 띄운다
         req.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
         req.add_header("Accept-Language", "ko-KR,ko;")
-    #   req.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36")
+
 
         data = ''
 
@@ -53,7 +63,7 @@ class Crawler:
             for i in range(0, len(data)):
                 hyperTitle.append( (link + data[i].find('a')['href']).strip()  )
 
-            info = Information(hyperTitle, title)
+            info = Information(hyperTitle, title, 1)
 
         except Exception as e:
             print('Exception ')
@@ -70,7 +80,7 @@ class Crawler:
         req = Request("http://hysoft.hanyang.ac.kr/modules/board/bd_list.html?id=wt_notice")# urllib.request 데이터를 보낼 때 인코딩하여 바이너리 형태로 보낸다 없는 페이지를 요청해도 에러를 띄운다
         req.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
         req.add_header("Accept-Language", "ko-KR,ko;")
-    #   req.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36")
+    
 
         data = ''
 
@@ -98,7 +108,7 @@ class Crawler:
              
                 hyperTitle.append((link + data[i].find('a')['href']).strip())
 
-            info = Information(hyperTitle, title)
+            info = Information(hyperTitle, title, 2)
 
             
         except Exception as e:
@@ -112,7 +122,6 @@ class Crawler:
         req = Request("http://eng.hanyang.ac.kr/people/notice.php")# urllib.request 데이터를 보낼 때 인코딩하여 바이너리 형태로 보낸다 없는 페이지를 요청해도 에러를 띄운다
         req.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
         req.add_header("Accept-Language", "ko-KR,ko;")
-    #   req.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36")
         data = ''
         try:
             
@@ -135,13 +144,13 @@ class Crawler:
             title = []
             for i in range(0, len(data) ):
                 title.append(data[i].get_text().strip())
-                print(title[i])
+                # print(title[i])
 
             hyperTitle = []
             for i in range(0, len(data)):
                 hyperTitle.append((link + (data[i])['href']).strip())
 
-            info = Information(hyperTitle, title)
+            info = Information(hyperTitle, title, 3)
 
 
         except Exception as e:
@@ -150,61 +159,51 @@ class Crawler:
 
         return info #string형식으로 반환.
 
-info = Crawler().getData3()
+cnt = 0
+info = []
+info1 = Crawler().getData1()
+
+info.append(info1)
 
 
-for i in range(0, len(info.hypertitle)):
-    print('info.hyperlink : ' + info.hypertitle[i])
-    print('info.title : ' + info.title[i])
+info2 = Crawler().getData2()
+info.append(info2)
 
+info3 = Crawler().getData3()
+info.append(info3)
 
-# dynamodb = boto3.resource('dynamodb')
+# cnt += len(info3.title)
+# print(cnt)
 
-# # print(dynamodb.get_item(TableName='Notification', Key={'id'}))
+# for i in range(0, len(info)):
+#     print(info[i])
+#     for j in range(0, len(info[i].hypertitle)):
+#         print('info.hyperlink : ' + info[i].hypertitle[j])
+#         print('info.title : ' + info[i].title[j])
+#         print('organization : ', end='')
+#         print(info[i].organization)
 
-# table = dynamodb.Table('Notification-iwrkzo6ufzfpxidyj5nch7lk5a-dev')
-# # print(table.table_status)
-
-
-# try:
-
-#     print(table.creation_date_time)
-
-#     # table.put_item(
-#     #     Item = {
-#     #         'title': 'asdf',
-#     #         'duration':'안녕하세요'
-#     #     }
-#     # )
-# except Exception as e:
-#     print(traceback.format_exc())
-
-
-
-# dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2', aws_access_key_id='AKIAWNLMFLQOXWJMQRXQ',aws_secret_access_key='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')
-# #end_point =  https://dynamodb.ap-northeast-2.amazonaws.com
-# table = dynamodb.Table('Notification')
-
-# client =boto3.client('dynamodb')
-
-# response = client.get_item(
-#     TableName = 'Notification',
-#     Key = {id:'1'}
-
-# )
-# try:
-#     dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')
-#     # , aws_access_key_id='AKIAWNLMFLQOXWJMQRXQ',aws_secret_access_key='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'
-# #end_point =  https://dynamodb.ap-northeast-2.amazonaws.com
-#     table = dynamodb.Table('Notification')
-
-#     table.put_item(Item={
-#         'id': 5,
-#         'content': '',
-#         'link':'안녕하세요'
-#     })
-# except Exception as e:
-#     print(e.response)
+session = boto3.Session(profile_name='bns')
+dynamodb = session.resource('dynamodb', region_name='ap-northeast-2')
+table = dynamodb.Table('Notification-iwrkzo6ufzfpxidyj5nch7lk5a-dev')
+cnt = 7
+try:
+    print('Put_Item')
+    for i in range(0, len(info)):
+        for j in range(0, len(info[i].hypertitle)):
+            # print()
+            table.put_item(
+                Item={
+                        'id': str(cnt),
+                        'link': info[i].hypertitle[j],
+                        'name': info[i].title[j],
+                        'notificationOrganizationId' : str(info[i].organization)
+                    }
+                )
+            cnt += 1
+            
+except Exception as e:
+    print(e.response)
 
 
 # e = SendingEmail.Email()
@@ -214,5 +213,3 @@ for i in range(0, len(info.hypertitle)):
 # while True:
 #     schedule.run_pending()
 #     time.sleep(1)
-
-
