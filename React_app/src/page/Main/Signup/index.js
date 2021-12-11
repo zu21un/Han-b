25b7cd888a9c513f0dfd1265e9fb159082e39ed2
ihyunmin <gusals005@gmail.com>
2021-12-12 01:34:56 +0900

    Docs: 폴더 구조 수정

---
+++ React_app/src/page/Main/Signup/index.js
@@ -0,0 +1,214 @@
+import React, { useState, useEffect } from 'react';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+
+import API from '@aws-amplify/api';
+import { getUser } from '../../../graphql/queries'
+
+import Header from './Header';
+import Setting from './Setting';
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link> {' '}
+//       {new Date().getFullYear()}.
+//     </Typography>
+//   );
+// }
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      light: '#63ccff',
+      main: '#009be5',
+      dark: '#006db3',
+    },
+  },
+  typography: {
+    h5: {
+      fontWeight: 500,
+      fontSize: 26,
+      letterSpacing: 0.5,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  components: {
+    MuiTab: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: 48,
+    },
+  },
+});
+
+theme = {
+  ...theme,
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: '#081627',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+        contained: {
+          boxShadow: 'none',
+          '&:active': {
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          marginLeft: theme.spacing(1),
+        },
+        indicator: {
+          height: 3,
+          borderTopLeftRadius: 3,
+          borderTopRightRadius: 3,
+          backgroundColor: theme.palette.common.white,
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          margin: '0 16px',
+          minWidth: 0,
+          padding: 0,
+          [theme.breakpoints.up('md')]: {
+            padding: 0,
+            minWidth: 0,
+          },
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(1),
+        },
+      },
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          borderRadius: 4,
+        },
+      },
+    },
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'rgb(255,255,255,0.15)',
+        },
+      },
+    },
+    MuiListItemButton: {
+      styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            color: '#4fc3f7',
+          },
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        primary: {
+          fontSize: 14,
+          fontWeight: theme.typography.fontWeightMedium,
+        },
+      },
+    },
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          color: 'inherit',
+          minWidth: 'auto',
+          marginRight: theme.spacing(2),
+          '& svg': {
+            fontSize: 20,
+          },
+        },
+      },
+    },
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          width: 32,
+          height: 32,
+        },
+      },
+    },
+  },
+};
+
+export default function SettingPage (props) {
+  const [user, setUser] = useState({})
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [myKeys, setMyKeys] = useState([]);
+
+  useEffect(() => {
+    setUser({id:props.userId})
+  }, [props])
+
+  useEffect(() => {
+      // email, user info setting
+      console.log('user use Effect Start!')
+      API.graphql({ query: getUser, variables:{ id: user.id }})
+      .then(res => {
+        console.log('Setting', res);
+        setName(res.data.getUser.name);
+        setEmail(res.data.getUser.email);
+        let mykeys_ = res.data.getUser.keywords.items;
+        setMyKeys(mykeys_.map((item) => item.keyword.name));
+      })
+      .catch(e => console.log(e));
+  }, [user]);
+
+  const handleLogin = (e) => {
+    props.navigate("/login")
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex', minHeight: '100vh', minWidth: '100%' }}>
+        <CssBaseline />
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+          <Header userName={name}/>
+          {/* <Header /> */}
+            <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
+              <Setting 
+                userId={user.id} 
+                userEmail={email}
+                userKeywords={myKeys}
+                handleLogin={handleLogin} />
+            </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
